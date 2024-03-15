ea98064b8eeb6cb5aba9cc5e35cd31e633bb4a6a
Denys Pieshkov <160874397+Pieshkov@users.noreply.github.com>
2024-03-15 18:33:50 +0000

    Goals

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,6 @@
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    const totalNumberOfGoalsInAllThreeLeagues =
+      laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return totalNumberOfGoalsInAllThreeLeagues;
+  }
+  
