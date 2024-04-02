33236cf943eb93cbd0a335b4bd18bed3a1f492ea
FlekserGithuber <60925388+FlekserGithuber@users.noreply.github.com>
2024-04-02 17:25:07 +0000

    json

--- js/coffee-machines-carousel.js
+++ js/coffee-machines-carousel.js
@@ -1,22 +1,32 @@
-const coffeeMachines = await fetch('api/machines.json');
-const articles = await coffeeMachines.json();;
+async function fetchCoffeeMachines() {
+    try {
+        const response = await fetch('api/machines.json');
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        const coffeeMachines = await response.json();
+        renderCoffeeMachines(coffeeMachines);
+    } catch (error) {
+        console.error('Error fetching coffee machines:', error);
+    }
+}
 
-function renderCoffeeMachines(coffeeMachinesList){
+function renderCoffeeMachines(coffeeMachinesList) {
     let machinesDomString = ``;
     for (const machine of coffeeMachinesList) {
         machinesDomString += `
-          <div class="coffee-machine__item">
-            <a href="#">
-            <img class="coffee-machine__img" src="${machine.image}" alt="${machine.title}">
-            <h6>${machine.title}</h6>
-            </a>
-            <p>${machine.price}</p>
-            <button class="coffee-machine__button">Add to Cart</button>
-        </div>
+            <div class="coffee-machine__item">
+                <a href="#">
+                    <img class="coffee-machine__img" src="${machine.image}" alt="${machine.title}">
+                    <h6>${machine.title}</h6>
+                </a>
+                <p>${machine.price}</p>
+                <button class="coffee-machine__button">Add to Cart</button>
+            </div>
         `;
     }
     const machinesContainer = document.querySelector('.coffee-machines__items-container');
     machinesContainer.innerHTML = machinesDomString;
 }
 
-renderCoffeeMachines(coffeeMachines);
+fetchCoffeeMachines();
